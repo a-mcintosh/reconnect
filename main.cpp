316a68b64a6b0ecaa7bd30ca4e9a79f81e2f2450
Aubrey McIntosh, PhD <aubrey.mcintosh@utexas.edu>
2018-08-11 23:33:24 -0500

    The rogue project, disconnected from it's ancestors.
    Signed-off-by: Aubrey McIntosh, PhD <aubrey.mcintosh@utexas.edu>

---
+++ main.cpp
@@ -0,0 +1,6 @@
+// This is the first commit in a project
+
+// This is some important change to the first commit.  I call it "second commit."
+
+// This is a rogue change to the project and it is not connected by git to the earlier project.
+
